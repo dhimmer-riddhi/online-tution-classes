9430c2b63d94f8d2a8d4ea180fc9b52831849db4
Riddhi Dhimmar <riddhidhimmar11@gmail.com>
2026-03-12 13:01:16 +0530

    craete student

--- src/app/firebase-service/firebase-enum.ts
+++ src/app/firebase-service/firebase-enum.ts
@@ -3,11 +3,11 @@ export enum FirebaseCollections {
   Applications = 'applications',
   Courses = 'courses',
   Payments = 'payments',
-  // Students = 'students',
   Teachers = 'teachers',
   StudentRegistrations = 'student-registrations', // ✅ ADD THIS
   Standard = 'stadards',
   ClassContent = "ClassContent",
-  Quiz = 'quiz'
+  Quiz = 'quiz',
+  Students = "Students"
 
 }
